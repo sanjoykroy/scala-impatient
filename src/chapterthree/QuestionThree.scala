087d81c17c762e5c6bb87c27ad4b16d8531ce0e7
unknown <pc@pc-PC.(none)>
2012-11-05 23:08:14 +0000

    Answers for Q1, Q2 and Q3 of chapter 3

---
+++ src/chapterthree/QuestionThree.scala
@@ -0,0 +1,12 @@
+package chapterthree
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pc
+ * Date: 05/11/12
+ * Time: 22:36
+ * To change this template use File | Settings | File Templates.
+ */
+object QuestionThree {
+
+}
