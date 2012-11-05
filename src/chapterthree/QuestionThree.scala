95978bf4cc648be63a2125d7dc5bb46240725845
sanjoykroy <sanjoykr78@hotmail.com>
2012-11-05 23:21:52 +0000

    Q3 of chapter 3

--- src/chapterthree/QuestionThree.scala
+++ src/chapterthree/QuestionThree.scala
@@ -1,12 +1,13 @@
 package chapterthree
 
 /**
- * Created with IntelliJ IDEA.
- * User: pc
- * Date: 05/11/12
- * Time: 22:36
- * To change this template use File | Settings | File Templates.
+ * Question - 3
+ * Repeat the preceding assignment, but produce a new array with the swapped values. Use for/yield.
  */
 object QuestionThree {
-
+  def main(args: Array[String]) {
+     val arr = Array(1, 2, 3, 4, 5)
+     val newArr = (for {b <- arr.grouped(2); c <- b.reverse} yield c).toArray
+     println(newArr.mkString(","))
+  }
 }
