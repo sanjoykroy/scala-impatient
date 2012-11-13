6403b096ad43482d40922a865bb9117e47f34740
sanjoykroy <sanjoykr78@hotmail.com>
2012-11-13 22:33:39 +0000

    Q3 of Chapter 4

---
+++ src/chapterfour/QuestionThree.scala
@@ -0,0 +1,17 @@
+package chapterfour
+
+/**
+ * Question - 3
+ */
+object QuestionThree {
+  def main(args: Array[String]) {
+    var words = Map[String, Int]()
+    val in = new java.util.Scanner(new java.io.File("D:\\development\\scalaProjects\\scala-impatient\\src\\chapterfour\\myFile.txt"))
+    while (in.hasNext) {
+      val word = in.next.toLowerCase
+      val count = words.getOrElse(word , 0)+1
+      words = words + (word -> count)
+    }
+    println(words.toString())
+  }
+}
