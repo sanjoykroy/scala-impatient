70fc386bbce4d726758971b17dbcc4c68225ff91
sanjoykroy <sanjoykr78@hotmail.com>
2012-11-17 20:45:05 +0000

    Q6 of Chapter 5

---
+++ src/chapterfive/Person.scala
@@ -0,0 +1,6 @@
+package chapterfive
+
+
+class Person(var age: Int) {
+  if (age < 0 ) age = 0
+}
---
+++ src/chapterfive/QuestionSix.scala
@@ -0,0 +1,9 @@
+package chapterfive
+
+
+object QuestionSix {
+  def main(args: Array[String]) {
+    val person = new Person(-12)
+    println(person.age)
+  }
+}
