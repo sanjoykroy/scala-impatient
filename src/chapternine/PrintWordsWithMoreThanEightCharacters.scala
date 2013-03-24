3a3bb97e6483a688d8c2ee54cddf7809e45d9022
sanjoykroy <sanjoykr78@hotmail.com>
2013-03-24 21:52:56 +0000

    Q3 of Chapter 9

--- src/chapternine/PrintWordsWithMoreThanEightCharacters.scala
+++ src/chapternine/PrintWordsWithMoreThanEightCharacters.scala
@@ -6,9 +6,8 @@ import io.Source
 object PrintWordsWithMoreThanEightCharacters {
   def main(args: Array[String]) {
     val source = Source.fromFile("D:\\myFile.txt")
-    for (token <- source.mkString.split("\\s+") if (token.length > 8)){
-        println(token)
-    }
+    val words = for (token <- source.mkString.split("\\s+") if (token.length > 8)) yield token
+    println(words.mkString("\n"))
     source.close()
   }
 }
