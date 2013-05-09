6f2d7c0eb9ce6b4ed1642480618e30cff7c548a6
sanjoykroy <sanjoykr78@hotmail.com>
2013-05-09 22:45:05 +0100

    Q3 of Chapter 11

---
+++ src/chaptereleven/QuestionThree.scala
@@ -0,0 +1,16 @@
+package chaptereleven
+
+
+
+object QuestionThree {
+  def main(args: Array[String]) {
+    val frac1 = Fraction(15, -6)
+    val frac2 = Fraction(10, 4)
+    println(frac1)
+    println(frac2)
+    println(frac1 + frac2)
+    println(frac1 - frac2)
+    println(frac1 * frac2)
+    println(frac1 / frac2)
+  }
+}
