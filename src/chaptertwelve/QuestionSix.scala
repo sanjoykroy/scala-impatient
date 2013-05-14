d4760091d64e3eaaaae5f3aca140a56663bd7c4c
sanjoykroy <sanjoykr78@hotmail.com>
2013-05-14 22:44:50 +0100

    Q6 of Chapter 12

---
+++ src/chaptertwelve/QuestionSix.scala
@@ -0,0 +1,12 @@
+package chaptertwelve
+
+
+object QuestionSix {
+  def main(args: Array[String]) {
+    println(largest(x => 10 * x - x * x, 1 to 10))
+  }
+
+  def largest(fun: (Int) => Int, inputs: Seq[Int]) = inputs.map(x => (x, fun(x))).reduceLeft(
+      (x, y) => if (x._2 > y._2) x else y
+   )._1
+}
